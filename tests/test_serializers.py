b911b72352cc84959b692f8da9b90602c6c8599a
Ryan P Kilby <kilbyr@gmail.com>
2019-08-01 16:59:59 -0700

    Make users directly accessible for testing

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -33,20 +33,14 @@ class ObjectPermissionsAssignmentIntegrationTests(TestCase):
     """
 
     def setUp(self):
-
         create = User.objects.create_user
-        self.users = {
-            'writer': create('writer', 'writer@example.com', 'password'),
-            'regular_user':
-                create('regular_user', 'regular_user@example.com', 'password'),
-            'reader': create('reader', 'reader@example.com', 'password'),
-        }
+        self.writer = create('writer', password='password')
+        self.reader = create('reader', password='password')
+        self.no_perms = create('no_perms', password='password')
 
-        create = Group.objects.create
-        self.groups = {
-            'readers': create(name='readers'),
-        }
-        self.groups['readers'].user_set.add(self.users['reader'])
+        # Add readers to our reader group
+        reader_group = Group.objects.create(name='readers')
+        reader_group.user_set.add(self.reader)
 
         model_name = BasicModel._meta.model_name
         self.perms = {
@@ -57,7 +51,7 @@ class ObjectPermissionsAssignmentIntegrationTests(TestCase):
 
     def create_object(self):
         request = factory.post('/')
-        request.user = self.users['writer']
+        request.user = self.writer
 
         serializer = BasicSerializer(
             data={
@@ -75,51 +69,33 @@ class ObjectPermissionsAssignmentIntegrationTests(TestCase):
     def test_can_read_assigned_objects(self):
         instance = self.create_object()
 
-        assert self.users['writer'].has_perm(self.perms['view'], instance)
+        assert self.writer.has_perm(self.perms['view'], instance)
         # check if readers group members have view perm
-        assert self.users['reader'].has_perm(self.perms['view'], instance)
+        assert self.reader.has_perm(self.perms['view'], instance)
 
     def test_can_change_assigned_objects(self):
         instance = self.create_object()
 
-        assert self.users['writer'].has_perm(self.perms['change'], instance)
+        assert self.writer.has_perm(self.perms['change'], instance)
 
     def test_cannot_read_unassigned_objects(self):
         instance = self.create_object()
 
-        assert not self.users['regular_user'].has_perm(
-            self.perms['view'],
-            instance,
-        )
+        assert not self.no_perms.has_perm(self.perms['view'], instance)
 
     def test_cannot_change_unassigned_objects(self):
         instance = self.create_object()
 
-        assert not self.users['regular_user'].has_perm(
-            self.perms['change'],
-            instance,
-        )
+        assert not self.no_perms.has_perm(self.perms['change'], instance)
         # check if readers group members don't have change perm
-        assert not self.users['reader'].has_perm(
-            self.perms['change'],
-            instance,
-        )
+        assert not self.reader.has_perm(self.perms['change'], instance)
 
     def test_cannot_delete_unassigned_objects(self):
         instance = self.create_object()
 
-        assert not self.users['writer'].has_perm(
-            self.perms['delete'],
-            instance,
-        )
-        assert not self.users['reader'].has_perm(
-            self.perms['delete'],
-            instance,
-        )
-        assert not self.users['regular_user'].has_perm(
-            self.perms['delete'],
-            instance,
-        )
+        assert not self.writer.has_perm(self.perms['delete'], instance)
+        assert not self.reader.has_perm(self.perms['delete'], instance)
+        assert not self.no_perms.has_perm(self.perms['delete'], instance)
 
 
 class ObjectPermissionsAssignmentImplementationTests(TestCase):
