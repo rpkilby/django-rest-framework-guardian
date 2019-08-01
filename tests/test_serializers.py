2f468b13f8ea279c1ccdfa281c5d211f9d202ef1
Adil Hemache <adilhemache@gmail.com>
2019-08-01 11:14:35 +0100

    Rewrite object permissions assignment tests to use the serializer directly instead of processing whole request/response

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -1,14 +1,11 @@
 
 from django.contrib.auth.models import Group, User
 from django.test import TestCase
-from rest_framework import authentication, serializers, status, viewsets
+from rest_framework import serializers
 from rest_framework.test import APIRequestFactory
 
-from rest_framework_guardian.filters import DjangoObjectPermissionsFilter
 from rest_framework_guardian.serializers import ObjectPermissionsAssignmentMixin
 from tests.models import BasicModel
-from tests.permissions import ViewObjectPermissions
-from tests.utils import basic_auth_header
 
 
 factory = APIRequestFactory()
@@ -29,162 +26,96 @@ class BasicSerializer(ObjectPermissionsAssignmentMixin, serializers.ModelSeriali
         }
 
 
-class BasicViewSet(viewsets.ModelViewSet):
-    queryset = BasicModel.objects.all()
-    serializer_class = BasicSerializer
-    authentication_classes = [authentication.BasicAuthentication]
-    permission_classes = [ViewObjectPermissions]
-    filter_backends = [DjangoObjectPermissionsFilter]
-
-
 class ObjectPermissionsAssignmentIntegrationTests(TestCase):
     """
     Integration tests for the object level permissions assignment API.
     """
 
     def setUp(self):
-        from guardian.shortcuts import assign_perm
 
-        # create users
         create = User.objects.create_user
-        users = {
-            'user1': create('user1', 'user1@example.com', 'password'),
-            'user2': create('user2', 'user2@example.com', 'password'),
-            'user3': create('user3', 'user2@example.com', 'password'),
+        self.users = {
+            'writer': create('writer', 'writer@example.com', 'password'),
+            'regular_user':
+                create('regular_user', 'regular_user@example.com', 'password'),
+            'reader': create('reader', 'reader@example.com', 'password'),
         }
 
+        create = Group.objects.create
+        self.groups = {
+            'readers': create(name='readers'),
+        }
+        self.groups['readers'].user_set.add(self.users['reader'])
+
         model_name = BasicModel._meta.model_name
-        app_label = BasicModel._meta.app_label
-
-        # give everyone model level permissions, as we are not testing those
-        everyone = Group.objects.create(name='everyone')
-        everyone.user_set.add(*users.values())
-
-        for action in ('add', 'view', 'change', 'delete'):
-            perm = '{0}.{1}_{2}'.format(app_label, action, model_name)
-            assign_perm(perm, everyone)
-
-        # readers are supposed to get `view` permission by the assigner
-        readers = Group.objects.create(name='readers')
-        readers.user_set.add(users['user3'])
-
-        perm = '{0}.{1}_{2}'.format(app_label, 'view', model_name)
-        assign_perm(perm, readers)
-
-        self.credentials = {}
-        for user in users.values():
-            self.credentials[user.username] = basic_auth_header(user.username, 'password')
-
-    def request(self, method, action, username, data=None, pk=None):
-        """
-        A shortcut function to facilitate requests to BasicViewSet
-        """
-
-        request = getattr(factory, method)(
-            '/',
-            data,
-            HTTP_AUTHORIZATION=self.credentials[username],
-        )
-        view = BasicViewSet.as_view({
-            method: action,
-        })
-        return view(request, pk=pk)
+        self.perms = {
+            'view': '{0}_{1}'.format('view', model_name),
+            'change': '{0}_{1}'.format('change', model_name),
+            'delete': '{0}_{1}'.format('delete', model_name),
+        }
 
     def create_object(self):
-        response = self.request(
-            'post',
-            'create',
-            'user1',
+        request = factory.post('/')
+        request.user = self.users['writer']
+
+        serializer = BasicSerializer(
             data={
-                'text': 'hello world',
+                'text': 'test',
+            },
+            context={
+                'request': request,
             },
         )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        serializer.is_valid(raise_exception=True)
+        instance = serializer.save()
+
+        return instance
 
     def test_can_read_assigned_objects(self):
-        self.create_object()
+        instance = self.create_object()
 
-        response = self.request(
-            'get',
-            'list',
-            'user1',
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-
-        response = self.request(
-            'get',
-            'list',
-            'user3',
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
+        assert self.users['writer'].has_perm(self.perms['view'], instance)
+        # check if readers group members have view perm
+        assert self.users['reader'].has_perm(self.perms['view'], instance)
 
     def test_can_change_assigned_objects(self):
-        self.create_object()
+        instance = self.create_object()
 
-        response = self.request(
-            'patch',
-            'partial_update',
-            'user1',
-            data={
-                'text': 'hello world 2',
-            },
-            pk=1,
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert self.users['writer'].has_perm(self.perms['change'], instance)
 
     def test_cannot_read_unassigned_objects(self):
-        self.create_object()
+        instance = self.create_object()
 
-        response = self.request(
-            'get',
-            'list',
-            'user2',
+        assert not self.users['regular_user'].has_perm(
+            self.perms['view'],
+            instance,
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 0)
 
     def test_cannot_change_unassigned_objects(self):
-        self.create_object()
+        instance = self.create_object()
 
-        response = self.request(
-            'patch',
-            'partial_update',
-            'user2',
-            data={
-                'text': 'hello world 2',
-            },
-            pk=1,
+        assert not self.users['regular_user'].has_perm(
+            self.perms['change'],
+            instance,
         )
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-        response = self.request(
-            'patch',
-            'partial_update',
-            'user3',
-            data={
-                'text': 'hello world 3',
-            },
-            pk=1,
+        # check if readers group members don't have change perm
+        assert not self.users['reader'].has_perm(
+            self.perms['change'],
+            instance,
         )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_cannot_delete_unassigned_objects(self):
-        self.create_object()
+        instance = self.create_object()
 
-        response = self.request(
-            'delete',
-            'destroy',
-            'user1',
-            pk=1,
+        assert not self.users['writer'].has_perm(
+            self.perms['delete'],
+            instance,
         )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-        response = self.request(
-            'delete',
-            'destroy',
-            'user3',
-            pk=1,
+        assert not self.users['reader'].has_perm(
+            self.perms['delete'],
+            instance,
         )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        assert not self.users['regular_user'].has_perm(
+            self.perms['delete'],
+            instance,
+        )
