0d35bf4ac38756711d2b854ddef4089922aaa046
Ernesto Chavez Sanchez <ejchavez88@gmail.com>
2020-06-27 20:53:36 +0000

    :zap: allow for django to cache model content type after first call

--- src/rest_framework_guardian/filters.py
+++ src/rest_framework_guardian/filters.py
@@ -8,7 +8,7 @@ class ObjectPermissionsFilter(BaseFilterBackend):
     A filter backend that limits results to those where the requesting user
     has read object level permissions.
     """
-    perm_format = '%(app_label)s.view_%(model_name)s'
+    perm_format = 'view_%(model_name)s'
     shortcut_kwargs = {
         'accept_global_perms': False,
     }
@@ -21,7 +21,6 @@ class ObjectPermissionsFilter(BaseFilterBackend):
 
         user = request.user
         permission = self.perm_format % {
-            'app_label': queryset.model._meta.app_label,
             'model_name': queryset.model._meta.model_name,
         }
 
